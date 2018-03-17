a0295d1d6beed7bf67e13ff5c79cd547b841c744
Wendy Xie <wxie@WXIE-MBP.nimblestorage.com>
2018-03-17 13:28:27 -0700

    clock

---
+++ my-clock/src/Clock.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Clock extends Component {
+    constructor() {
+        super();
+        this.state = {
+            date: new Date()
+        }
+    };
+    componentWillMount() {
+        this.timer = setInterval(() => {
+            this.setState({date : new Date()})
+        }, 1000)
+    };
+    render() {
+        return(
+            <div>
+                <p>This current time is: </p>
+                {this.state.date.toLocaleTimeString()}
+            </div>
+        )
+    }
+
+}
+
+export default Clock;
