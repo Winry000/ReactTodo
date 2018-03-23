b6a9f60604915c61f8f33be432e5eb3bc6dedae5
Wendy Xie <winry830love@gmail.com>
2018-03-22 22:16:58 -0700

    toggle

--- my-test/src/App.js
+++ my-test/src/App.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import Board from './Board.js'
 
+
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      toggle: false,
+      name: 'start'
+    }
+  }
+  toggleButton(){
+    const { toggle, name } = this.state;
+    this.setState({
+      toggle : !toggle,
+      name : (toggle ? 'Start' : 'End')
+    });
+  }
   render() {
     return (
       <div className="game">
-        <div className="gameboard">
-          <Board/>
-        </div>
+        <button onClick={this.toggleButton.bind(this)}>{this.state.name}</button>
+        { this.state.toggle && 
+          (
+            <div className="gameboard">
+              <Board/>
+            </div>
+          )
+        }
       </div>
     );
   }
